8e9a29f1d6ec0e7dbf1818b6265f549209972e26
Alvaro <alvaro.munoz@caplin.com>
2017-10-18 19:03:02 +0100

    Implemented offer/answer between client/server -> working

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,5 +1,11 @@
 const AppConfig = {
-	SIGNALING_SERVER_URL: 'http://localhost:8888'
+	SIGNALING_SERVER_URL: 'http://localhost:8888',
+	PEER_CONNECTION_CONFIG: {
+		iceServers: [{'urls': 'stun:stun.l.google.com:19302'}]
+	},
+	PEER_CONNECTION_CONSTRAINTS: {
+		optional: []
+	}
 };
 
 export default AppConfig;
